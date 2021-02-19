20bbd19e3d39aa27d64519d0adef4e185f7e053f
Juan Carlos Cárdenas <juancarlos.cardenas@aesir.com.mx>
2021-02-19 15:14:03 -0600

    Pantalla de index y home

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <?php include_once('./template/head.php') ?>
+</head>
+<body>
+    <?php include_once('./template/header.php') ?>
+    <section id="home" class="row h-100 w-100 mx-auto d-desktop">
+        <div class="col-xs-12 col-md-6 bg-home-img-team">
+            <div class="d-flex align-items-center justify-content-center h-100">
+                <div>
+                    <h1 class="text-uppercase text-head w450">inigualable</h1>
+                    <h1 class="text-uppercase text-head w500">incomparable</h1>
+                    <h1 class="text-uppercase text-head w350">increíble</h1>
+                    <button class="col-12 col-md-6 btn btn-blue text-uppercase mt-4">Contrata hoy</button>
+                </div>
+            </div>
+        </div>
+        <div class="col-xs-12 col-md-6 bg-home-img-guy">
+            <div class="w-100 h-100 d-flex align-items-center">
+                <div class="w-80 mx-auto">
+                    <button class="col-7 btn btn-orange text-uppercase mt-50">Eres cliente y necesitas ayuda</button>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="homeMobile" class="h-100 w-100 mx-auto d-mobile">
+        <div id="carouselExampleControls" class="carousel slide h-100" data-bs-ride="carousel">
+            <div class="carousel-inner h-100">
+                <div class="carousel-item active h-100">
+                    <div class="col-12 col-md-6 bg-home-img-team">
+                        <div class="d-flex align-items-center justify-content-center h-100">
+                            <div>
+                                <h1 class="text-uppercase text-head w450">inigualable</h1>
+                                <h1 class="text-uppercase text-head w500">incomparable</h1>
+                                <h1 class="text-uppercase text-head w350">increíble</h1>
+                                <button class="col-12 col-md-6 btn btn-blue text-uppercase mt-4">Contrata hoy</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item h-100">
+                    <div class="col-12 col-md-6 bg-home-img-guy">
+                        <div class="w-100 h-100 d-flex align-items-center">
+                            <div class="w-80 mx-auto">
+                                <button class="col-12 col-md-7 btn btn-orange text-uppercase mt-50">Eres cliente y necesitas ayuda</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls"  data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls"  data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+    </section>
+    <?php include_once('./template/footer.php') ?>
+    <?php include_once('./template/scripts.php') ?>
+</body>
+</html>
---
+++ template/header.php
@@ -0,0 +1,10 @@
+<nav>
+    <div class="col-md-11 mx-auto row">
+        <div class="col-1 d-flex align-items-center">
+            <i class="fas fa-phone-volume fs-45 c-blue"></i>
+        </div>
+        <div class="col-11 text-center">
+            <img src="./assets/images/logo.png" alt="">
+        </div>
+    </div>
+</nav>
